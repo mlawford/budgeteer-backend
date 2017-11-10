cd6be8139936b4709c93ff789a1d941efc22093c
Ryan Sperzel <ryansperzel@gmail.com>
2017-11-10 10:22:47 -0500

    cors fix

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,6 +10,11 @@ class Api::UsersController < ApplicationController
     @user = User.create(user_params)
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
