8b0f75f098ac7cb6da4f47398369909808cd44d3
Ryan Sperzel <ryansperzel@gmail.com>
2017-11-13 17:17:41 -0500

    Category Budget Serializer Fix V1

--- app/serializers/category_budget_serializer.rb
+++ app/serializers/category_budget_serializer.rb
@@ -1,4 +1,4 @@
-class CategoryBudgetsSerializer < ActiveModel::Serializer
+class CategoryBudgetSerializer < ActiveModel::Serializer
   attributes :id, :category_name, :category_budget_total, :monthly_budget_id
   has_many :transactions
   belongs_to :monthly_budget
